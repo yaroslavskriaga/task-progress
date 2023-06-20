97948a5834d738e2358c1da7a7a2ee18e7c90034
yaroslavskriaga <yaroslav.skriaga@googlemail.com>
2023-06-20 21:09:53 +0200

    (fix): optimised progress calc func, added test cases for optimised funcs

--- src/modules/profile/utils/profile-helpers.ts
+++ src/modules/profile/utils/profile-helpers.ts
@@ -1,21 +1,30 @@
 import { TaskGroupInterface, TaskInterface } from "shared/api/task/task";
 import { Nullable } from "shared/api/rest";
 
+export function calculateNormalizedValue(sumValues: number, sumCheckedValues: number): number {
+  return (sumCheckedValues * 100) / sumValues;
+}
+
+export function calculateSumValues(tasks: TaskInterface[]): { sumValues: number; sumCheckedValues: number } {
+  let sumValues = 0;
+  let sumCheckedValues = 0;
+
+  tasks.forEach((task: TaskInterface) => {
+    sumValues += task.value;
+    if (task.checked) {
+      sumCheckedValues += task.value;
+    }
+  });
+
+  return { sumValues, sumCheckedValues };
+}
+
 export function calculateProgress(tasksList: Nullable<TaskGroupInterface[]>): number {
   const normalizedValues: number[] = [];
 
   tasksList?.forEach((task_group: TaskGroupInterface) => {
-    let sumValues = 0;
-    let sumCheckedValues = 0;
-
-    task_group.tasks.forEach((task: TaskInterface) => {
-      sumValues += task.value;
-      if (task.checked) {
-        sumCheckedValues += task.value;
-      }
-    });
-
-    const normalizedValue = (sumCheckedValues * 100) / sumValues;
+    const { sumValues, sumCheckedValues } = calculateSumValues(task_group.tasks);
+    const normalizedValue = calculateNormalizedValue(sumValues, sumCheckedValues);
     normalizedValues.push(Math.floor(normalizedValue));
   });
 
--- src/modules/profile/utils/profile-helpres.test.ts
+++ src/modules/profile/utils/profile-helpres.test.ts
@@ -1,8 +1,27 @@
-import { calculateProgress } from "./profile-helpers";
+import { calculateNormalizedValue, calculateProgress, calculateSumValues } from "./profile-helpers";
+import { TaskGroupInterface, TaskInterface } from "shared/api/task/task";
+
+describe("calculateSumValues", () => {
+  it("calculateSumValues() calculates the sum of values and sum of checked values correctly", () => {
+    const tasks: TaskInterface[] = [
+      { value: 50, checked: true, description: "" },
+      { value: 100, checked: false, description: "" },
+      { value: 75, checked: true, description: "" },
+    ];
+
+    const expectedSumValues = 225;
+    const expectedSumCheckedValues = 125;
+
+    const { sumValues, sumCheckedValues } = calculateSumValues(tasks);
+
+    expect(sumValues).toBe(expectedSumValues);
+    expect(sumCheckedValues).toBe(expectedSumCheckedValues);
+  });
+});
 
 describe("calculateProgress", () => {
   it("calculateProgress() calculates progress bar value correctly", () => {
-    const tasksList = [
+    const tasksList: TaskGroupInterface[] = [
       {
         name: "Group 1",
         tasks: [
@@ -21,7 +40,18 @@ describe("calculateProgress", () => {
     ];
 
     const result = calculateProgress(tasksList);
-
     expect(result).toBe(50);
+  });
+});
+
+describe("calculateNormalizedValue", () => {
+  it("calculateNormalizedValue() calculates the normalized value correctly", () => {
+    const sumValues = 200;
+    const sumCheckedValues = 150;
+    const expectedNormalizedValue = 75;
+
+    const normalizedValue = calculateNormalizedValue(sumValues, sumCheckedValues);
+
+    expect(normalizedValue).toBe(expectedNormalizedValue);
   });
 });
